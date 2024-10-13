1bb3f79179d4331aa5a4d15579289660d7b8f98e
Anastasiya Volokitina <n060600@yandex.ru>
2024-10-13 23:04:08 +0500

    Region card

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import CardsRegion from "@/components/cards/region";
 
 export default function Home() {
   return (
     <div>
 MAIN page
+<CardsRegion />
+
     </div>
   );
 }
