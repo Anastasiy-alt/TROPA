6c5182ecd61f2f480c62a68a90bb0d04ee4e7b73
Anastasiya Volokitina <n060600@yandex.ru>
2024-11-07 14:12:57 +0500

    правки

--- src/app/photo/[id]/PhotoClient.jsx
+++ src/app/photo/[id]/PhotoClient.jsx
@@ -1,38 +1,37 @@
 "use client"
 import styles from './page.module.sass'
-import React, {useEffect, useRef, useState} from "react";
-import {useParams} from 'next/navigation';
-import {PhotoCards} from "@/const";
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from 'next/navigation';
+import { PhotoCards } from "@/const";
 import InfoDetailPhoto from '@/components/photoDetail/info'
 import SliderDetailPhoto from '@/components/photoDetail/slider'
 import BackButton from "@/components/elements/back";
 
 export default function PhotoClient() {
-
-    const router = useParams();
-    const id = router.id;
+    const { id } = useParams(); // Измените на деструктуризацию
     const [foundPhoto, setFoundPhoto] = useState(null);
     const imgRef = useRef(null);
     const [isVertical, setIsVertical] = useState(null);
-    const findPhotById = (data) => {
-        return data.PhotoCards.find(photo => photo.id === data.id);
+
+    const findPhotById = (id) => {
+        return PhotoCards.find(photo => photo.id === id); // Упростите функцию
     };
 
     const handleImageLoad = () => {
         if (imgRef.current) {
             const { naturalWidth, naturalHeight } = imgRef.current;
-            setIsVertical(naturalWidth < naturalHeight)
+            setIsVertical(naturalWidth < naturalHeight);
         }
     };
 
     useEffect(() => {
         if (id) {
-            const photo = findPhotById({PhotoCards, id});
-            setFoundPhoto(photo)
+            const photo = findPhotById(id); // Просто передайте id
+            setFoundPhoto(photo);
         }
     }, [id]);
 
-    return(
+    return (
         <div className='stock'>
             <BackButton />
             {foundPhoto && (
@@ -41,11 +40,11 @@ export default function PhotoClient() {
                         ref={imgRef}
                         onLoad={handleImageLoad}
                         src={foundPhoto.img}
-                        alt="" className={styles["main-info__img"]}/>
-                    <InfoDetailPhoto image={foundPhoto}/>
+                        alt="" className={styles["main-info__img"]} />
+                    <InfoDetailPhoto image={foundPhoto} />
                 </div>
             )}
             <SliderDetailPhoto photo={foundPhoto} />
         </div>
-    )
+    );
 }
